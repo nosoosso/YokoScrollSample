755fb99849a4b85aba0806ba29b88146479ae599
nosoosso <nosoosso.null@gmail.com>
2015-02-15 03:21:30 +0900

    敵キャラクターの削除

--- Assets/Scripts/MonoBehaviour/God.cs
+++ Assets/Scripts/MonoBehaviour/God.cs
@@ -13,6 +13,5 @@ public class God : MonoBehaviour
 		Instantiate(gameObjects[2], new Vector3(0, -2), Quaternion.identity);
 		Instantiate(gameObjects[2], new Vector3(1.57f, 0), Quaternion.identity);
 		Instantiate(gameObjects[2], new Vector3(-2, -1), Quaternion.identity);
-		Instantiate(gameObjects[3], new Vector3(1.17f, 3.21f), Quaternion.identity);
 	}
 }
